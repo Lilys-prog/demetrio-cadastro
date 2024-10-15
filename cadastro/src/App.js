19982ea61059b1708f38667ff5848556207996cb
Lily Silva <lilys.prog@gmail.com>
2024-10-15 17:46:07 -0300

    logo da sespa adicionado

--- cadastro/src/App.js
+++ cadastro/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import sespa from './img/sespa.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <div>
+          <img src="sespa" alt="logo da SESPA"></img>
+        </div>
       </header>
     </div>
   );
