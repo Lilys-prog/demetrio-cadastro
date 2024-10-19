89fd20d7d94a689a04556522ea5b3abb3cb1a8b8
Lily Silva <lilys.prog@gmail.com>
2024-10-19 08:36:59 -0300

    first component

--- cadastro/src/App.js
+++ cadastro/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import sespa from './img/sespa.jpeg'
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <img src="sespa" alt="logo da SESPA"></img>
+        <div className='logo'>
+          <img src={sespa} alt='logo da sespa' width='400px'></img>
+          <h1>Cadastro de Servidores</h1>
         </div>
       </header>
     </div>
